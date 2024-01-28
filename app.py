c0572ac075ca8308562b712cfd412e3ef0342152
Naman Rajendra Joshi <namanraj@usc.edu>
2024-01-28 01:41:14 -0800

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def init_retriever():
 with st.sidebar:   
     
     cohere_key = st.text_input("Enter Cohere API key", type="password")
-    #os.environ["COHERE_API_KEY"] = cohere_key
+    os.environ["COHERE_API_KEY"] = cohere_key
     gemini_key = st.text_input("Enter Google Gemini API key", type="password")
     
     pinecone_key = st.text_input("Enter Pinecone API key", type="password")
