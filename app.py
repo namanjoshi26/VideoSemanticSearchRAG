3d30163d6080f96c46108ff50fa3999f018386b8
Naman Rajendra Joshi <namanraj@usc.edu>
2024-01-29 16:58:35 -0800

    Update app.py

--- app.py
+++ app.py
@@ -114,10 +114,7 @@ def card(thumbnail, title, url, is_even):
     
 # Title section
 import streamlit as st
-st.markdown("<h1 style='text-align: center;'>Some title</h1>", unsafe_allow_html=True)
-st.title("""
-                    # Semantica Vision 
-""")
+st.markdown("<h1 style='text-align: center;'>Semantica Vision</h1>", unsafe_allow_html=True)
 st.write("""
 ### Semantic routing based Chatbot and Semantic Video Search without any Hallucinations
 """)
