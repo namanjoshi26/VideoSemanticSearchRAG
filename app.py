f1924d617b07c78d64a6fcf7463aefb2238a370c
Naman Rajendra Joshi <namanraj@usc.edu>
2024-01-29 16:41:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ st.write("""
 # Description section
 st.write("""###### It has knowledge(limited) about Artificial Intelligence/Machine Learning/Communications  
     Note: Multiple concurrent requests on multiple devices will lead to error. So try again later if that so 
-    No. of requests per minute are limited and hence that can lead to error too. 
+    No. of requests per minute are limited and hence that can lead to error too. So try again later if that so 
     Reason: Free Trial""")
 
 # Add some space between sections
