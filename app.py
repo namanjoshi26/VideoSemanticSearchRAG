a738eea6c6f5803bd6330e985a86d430faff7bf6
Naman Rajendra Joshi <namanraj@usc.edu>
2024-01-31 12:41:57 -0800

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ st.write("""###### It has knowledge(limited) about Artificial Intelligence/Machi
 # Add some space between sections
 st.write("USE CASE 1 - Out of Context Queries - Will say that it doesn't know eg. How to make a salad?")
 st.write("USE CASE 2 - In context Queries - Will summarize and suggest videos with videos starting at the exact timestamp where it is related to the query")
-st.write("USE CASE 3 - In Context but restricted topics - currently it supports - Politics, chitchat and personal opinions")  # Empty line for spacing
+st.write("USE CASE 3 - In Context but restricted topics - currently it supports - Politics, chitchat and personal opinions - will respond that it cannot talk about it")  # Empty line for spacing
 
 # Ask me a question section
 st.write("### Ask me a question!")
